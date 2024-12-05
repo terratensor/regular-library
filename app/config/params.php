f8c9b58df21ad952b829cd9303c8e8ed01bb5d98
agusev <audetv@gmail.com>
2024-12-05 19:42:31 +0300

    Добавлены параметры поумолчанию

--- app/config/params.php
+++ app/config/params.php
@@ -4,24 +4,24 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'cookieDomain' => getenv('COOKIE_DOMAIN'),
-    'frontendHostInfo' => getenv('FRONTEND_URL'),
-    'githubRepositoryUrl' => getenv('GH_REPO_URL'),
-    'urlShortenerHost' => getenv('URL_SHORTENER_HOST'), // Хост в сети интернет, в локальной сети docker - это наименования сервиса
-    'urlShortenerUrl' => getenv('URL_SHORTENER_URL'), // Хост в сети интернет, в локальной сети docker - это наименования сервиса
-    'shortLinkEnable' => getenv('SHORT_LINK_ENABLE'),
-    'cleanDesign' => getenv('CLEAN_DESIGN'), // default 0 не показывать меню сайта СВОДД, ничего лишнего
+    'cookieDomain' => getenv('COOKIE_DOMAIN') ?? 'localhost',
+    'frontendHostInfo' => getenv('FRONTEND_URL') ?? 'http://localhost:8030',
+    'githubRepositoryUrl' => getenv('GH_REPO_URL') ?? 'https://github.com/terratensor/regular-library',
+    'urlShortenerHost' => getenv('URL_SHORTENER_HOST') ?? 'url-shortener:8000', // Хост в сети интернет, в локальной сети docker - это наименования сервиса
+    'urlShortenerUrl' => getenv('URL_SHORTENER_URL') ?? 'http://localhost', // Хост в сети интернет, в локальной сети docker - это наименования сервиса
+    'shortLinkEnable' => getenv('SHORT_LINK_ENABLE') ?? 0,
+    'cleanDesign' => getenv('CLEAN_DESIGN') ?? 1, // default 0 не показывать меню сайта СВОДД, ничего лишнего
     'manticore' => [
         'host' => 'manticore',
         'port' => 9308,
         'max_matches' => getenv('MANTICORE_MAX_MATCHES') ?? 0, // Maximum amount of matches that the server keeps in RAM for each table and can return to the client. Default is unlimited.
     ],
     'searchResults' => [
-        'pageSize' => (int)getenv('PAGE_SIZE'),
+        'pageSize' => (int)getenv('PAGE_SIZE') ?? 50,
     ],
     'indexes' => [
-        'common' =>  getenv('MANTICORE_DB_NAME_COMMON'),
-        'concept' => getenv('MANTICORE_DB_NAME_COMMON') . '_concept',
+        'common' =>  getenv('MANTICORE_DB_NAME_COMMON') ?? 'library',
+        'concept' => getenv('MANTICORE_DB_NAME_COMMON') . '_concept' ?? 'library_concept',
     ],
     'bsVersion' => '5.x'
 ];
