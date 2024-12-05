ea274c4ea4e8c19d61f274407087b6e371c0c9be
audetv <algsound@yandex.ru>
2024-12-05 21:57:12 +0300

    добавлены локальные конфиги окружения

--- app/config/web.php
+++ app/config/web.php
@@ -15,8 +15,7 @@ $config = [
     ],
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => getenv('COOKIE_VALIDATION_KEY_FILE') ?? '',
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation        
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
